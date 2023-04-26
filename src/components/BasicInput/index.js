58a1672bba07e8e2d8818e980748c52f979299f7
ethanshealey <ethan.shealey@gmail.com>
2023-04-25 22:13:59 -0400

    hide keyboard on mobile

--- src/components/BasicInput/index.js
+++ src/components/BasicInput/index.js
@@ -3,7 +3,7 @@ import { useState } from 'react'
 const BasicInput = (props) => {
   return (
     <div className='basic-calc-input'>
-      <input value={props.equation} onChange={props.handleInput} onKeyDown={props.handleKeyDown} onfocus="blur();" id='basic-calc-input-id' />
+      <input value={props.equation} onChange={props.handleInput} onKeyDown={props.handleKeyDown} readonly="readonly" id='basic-calc-input-id' />
       <div className='basic-calc-answer' id='calc-ans'>{ props.answer }</div>
     </div>
   )
