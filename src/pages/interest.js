31e47d8d909efcb11a31ee7235eb58ef15cc5621
ethanshealey <ethan.shealey@gmail.com>
2023-04-27 18:54:08 -0400

    make interest chart prettier

--- src/pages/interest.js
+++ src/pages/interest.js
@@ -3,7 +3,7 @@ import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContai
 import { Input, Button } from 'antd';
 
 const numToMonth = (mo) => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'][mo%12]
-const getMonth = (i) => `${numToMonth(new Date().getMonth()+i)} '${(parseInt(new Date().getFullYear()) + Math.floor(i/12)).toString().slice(2)}`
+const getMonth = (i) => `${numToMonth(new Date().getMonth()+i)} '${(parseInt(new Date().getFullYear()) + (Math.floor(i/12) + new Date().getMonth())).toString().slice(2)}`
 
 const formatter = new Intl.NumberFormat('en-US', {
   style: 'currency',
@@ -46,10 +46,9 @@ const Interest = () => {
       if(totals.length !== 0) currInt += totals.slice(-1)[0].Interest
       currCont = Math.round(parseFloat(monthly) * (i+1))
 
-      totals.push({ key: i, month: getMonth(i), Total: currAmt, Interest: currInt, Contributions: currCont })
+      totals.push({ key: i, month: getMonth(i), Total: currAmt, Contributions: currCont, Interest: currInt })
     }
     setTotalAmounts(_ => totals)
-    console.log(totals)
   }
 
   return (
@@ -57,17 +56,17 @@ const Interest = () => {
       <div id='interest'>
         <div id='interest-input-panel'>
           <h2>Initial deposit:</h2>
-          <Input className='interest-input-dollar' addonBefore={'$'} defaultValue={'1000.00'} onChange={(e) => setInitial(_ => e.target.value)} />
+          <Input className='interest-input-dollar' addonBefore={'$'} value={initial} onChange={(e) => setInitial(_ => e.target.value)} />
           <h2>Monthly contribution:</h2>
-          <Input className='interest-input-dollar' addonBefore={'$'} defaultValue={'100.00'} onChange={(e) => setMonthly(_ => e.target.value)} />
+          <Input className='interest-input-dollar' addonBefore={'$'} value={monthly} onChange={(e) => setMonthly(_ => e.target.value)} />
           <h2>Over a period of:</h2>
           <span id="interest-input-time-wrapper">
-            <Input className='interest-input-time' defaultValue={'12'} onChange={(e) => setPeriod(_ => e.target.value)} />
+            <Input className='interest-input-time' value={period} onChange={(e) => setPeriod(_ => e.target.value)} />
             <div className='time-picker-item time-picker-item-active' id="months" onClick={() => changeTimePeriodType('months')}>Months</div>
             <div className='time-picker-item' id="years" onClick={() => changeTimePeriodType('years')}>Years</div>
           </span>
           <h2>APY:</h2>
-          <Input className='interest-input-apy' addonAfter={'%'} defaultValue={'4.15'} onChange={(e) => setRate(_ => e.target.value)} />
+          <Input className='interest-input-apy' addonAfter={'%'} value={rate} onChange={(e) => setRate(_ => e.target.value)} />
           <Button id='calculate-interest' type='primary' onClick={calculate} >Calculate</Button>
         </div>
         <div id='interest-result-panel'>
@@ -77,7 +76,6 @@ const Interest = () => {
               <span>
                 <ResponsiveContainer width="100%" height="100%">
                   <AreaChart
-                    width={500}
                     height={400}
                     data={totalAmounts}
                     margin={{
@@ -87,13 +85,27 @@ const Interest = () => {
                       bottom: 0,
                     }}
                   >
+                    <defs>
+                      <linearGradient id="colorTotal" x1="0" y1="0" x2="0" y2="1">
+                        <stop offset="5%" stopColor="#1677ff" stopOpacity={0.8}/>
+                        <stop offset="95%" stopColor="#1677ff" stopOpacity={0}/>
+                      </linearGradient>
+                      <linearGradient id="colorInterest" x1="0" y1="0" x2="0" y2="1">
+                        <stop offset="5%" stopColor="#ffc658" stopOpacity={0.8}/>
+                        <stop offset="95%" stopColor="#ffc658" stopOpacity={0}/>
+                      </linearGradient>
+                      <linearGradient id="colorContributions" x1="0" y1="0" x2="0" y2="1">
+                        <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>
+                        <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
+                      </linearGradient>
+                    </defs>
                     <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="month" />
+                    <XAxis dataKey="month"/>
                     <YAxis />
-                    <Tooltip />
-                    <Area type="monotone" dataKey="Total" stroke="#1677ff" fill="#1677ff" />
-                    <Area type="monotone" dataKey="Interest" stroke="#ffc658" fill="#ffc658" />
-                    <Area type="monotone" dataKey="Contributions" stroke="#82ca9d" fill="#82ca9d" />
+                    <Tooltip formatter={(v) => formatter.format(v)} />
+                    <Area type="monotone" dataKey="Total" stroke="#1677ff" fill="url(#colorTotal)" />
+                    <Area type="monotone" dataKey="Interest" stroke="#ffc658" fill="url(#colorInterest)" />
+                    <Area type="monotone" dataKey="Contributions" stroke="#82ca9d" fill="url(#colorContributions)" />
                   </AreaChart>
                 </ResponsiveContainer>
               </span>
@@ -107,15 +119,15 @@ const Interest = () => {
                 </h4>
                 <hr />
                 <h4 className='interest-breakdown-row'>
-                  <div id='interest-sq'></div> 
-                  <div>Total Interest</div> 
-                  <div>{ totalAmounts.length > 0 && formatter.format(totalAmounts?.slice(-1)[0].Interest) }</div>
-                </h4>
-                <hr />
-                <h4 className='interest-breakdown-row'>
                   <div id='contributions-sq'></div> 
                   <div>Total Contributions</div> 
                   <div>{ totalAmounts.length > 0 && formatter.format(totalAmounts?.slice(-1)[0].Contributions) }</div>
+                </h4>
+                <hr />
+                <h4 className='interest-breakdown-row'>
+                  <div id='interest-sq'></div> 
+                  <div>Total Interest</div> 
+                  <div>{ totalAmounts.length > 0 && formatter.format(totalAmounts?.slice(-1)[0].Interest) }</div>
                 </h4>
               </div>
             </>
